d3dce1ff0120b49d70b88bf3be8cd9395565a979
Akshaya421 <akshayagunda221@gmail.com>
2025-11-15 19:45:42 +0530

    Update pattern11.java

--- pattern11.java
+++ pattern11.java
@@ -1,22 +1,27 @@
-public class pattern11 {
-    public static void main(String args[]){
-        int n=5;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(i==2 && j==2 || i==2 && j==3 ||i==2 && j==4  ){
-                    System.out.print("  ");
-                }
-                else if(i==3 && j==2 || i==3 && j==3 ||i==3 && j==4 ){
-                     System.out.print("  ");
-                }
-                else if(i==4 && j==2 || i==4 && j==3 ||i==4 && j==4 ){
-                     System.out.print("  ");
-                }else{
-                    System.out.print("* ");
-                }
+class Main {
+    public static void main(String[] args) {
+        int n = 5; // number of rows
 
-            }System.out.println();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+
+                // Print stars on first row, last row, first column, last column
+                if(i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  "); // spaces inside
+                }
+            }
+            System.out.println();
         }
     }
-    
 }
+
+/*
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * *
+*/
+
